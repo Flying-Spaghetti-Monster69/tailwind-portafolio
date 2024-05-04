8d18bc38e850700c515770967f887217b34edb48
Felipe Garzon Melguizo <felipegarzon908@gmail.com>
2024-05-03 23:52:11 -0500

    align-element class added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { links } from "../data";
 const Navbar = () => {
   return (
     <nav className="bg-zinc-800">
-      <div className="mx-auto max-w-7xl px-8 py-4 flex flex-col sm:flex-row sm:gap-x-16 sm:items-center sm:py-8">
+      <div className="align-element py-4 flex flex-col sm:flex-row sm:gap-x-16 sm:items-center sm:py-8">
         <h2 className="text-3xl font-bold text-zinc-50">
           Web<span className="text-zinc-400">Dev</span>
         </h2>
